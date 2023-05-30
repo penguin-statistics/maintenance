8749a372ea456a96f2577619d6221a8a5e461635
GalvinGao <me@galvingao.com>
2023-05-31 02:24:20 +0800

    feat: shared resources

--- vite.config.ts
+++ vite.config.ts
@@ -20,5 +20,5 @@ export default defineConfig({
       },
     },
   },
-  base: "https://penguin.upyun.galvincdn.com/maintenance/v1.1.3/",
+  base: "https://penguin.upyun.galvincdn.com/maintenance/v1-shared/",
 });
